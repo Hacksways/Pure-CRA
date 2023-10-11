a5cb325007cf1764619cf4bd40d476fdcac5a053
Алексей <73067946+BlackLightninGg@users.noreply.github.com>
2023-10-11 18:01:27 +0000

    Add text-field for search decks

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -7,20 +7,30 @@ import {
   TableHeadCell,
   TableRow,
 } from "components/ui/table/table";
+import { TextField } from "components/ui/text-field";
 import { Typography } from "components/ui/typography";
+import { ChangeEvent, useState } from "react";
 import { useCreateDeckMutation, useGetDecksQuery } from "services/decks";
 import s from "./decks.module.scss";
 
 export const Decks = () => {
-  const { data } = useGetDecksQuery();
+  const [name, setName] = useState<string>("");
+  const [currentPage, setcurrentPage] = useState<number>(1);
+
+  const { data } = useGetDecksQuery({ name, currentPage });
   const [createDeck] = useCreateDeckMutation();
 
   const onClickAddNewDeckButton = () => {
     createDeck({ name: "Temp test deck" });
   };
 
+  const onChangeSearchTextField = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+  };
+
   return (
     <div className={s.pageDeck}>
+      <TextField isSearch value={name} onChange={onChangeSearchTextField} />
       <Button className={s.button} onClick={onClickAddNewDeckButton}>
         <Typography variant="subtitle2" as="span">
           Add new Deck
