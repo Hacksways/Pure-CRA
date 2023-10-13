5356de613dbf0d0f45b043f58909a4368c1bfa6d
Алексей <73067946+BlackLightninGg@users.noreply.github.com>
2023-10-13 13:50:32 +0000

    feat: delete deck via server#26
    refactor: change style modal

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,4 +1,6 @@
 import { Button } from "components/ui/button";
+import { Card } from "components/ui/card";
+import { Modal } from "components/ui/modal/modal";
 import {
   Table,
   TableBody,
@@ -11,15 +13,22 @@ import { TextField } from "components/ui/text-field";
 import { Typography } from "components/ui/typography";
 import { ChangeEvent, useState } from "react";
 import {
+  Deck,
   useCreateDeckMutation,
   useDeleteDeckMutation,
   useGetDecksQuery,
 } from "services/decks";
 import s from "./decks.module.scss";
 
+type CurrentDeck = Pick<Deck, "id" | "name">;
+
 export const Decks = () => {
   const [name, setName] = useState<string>("");
   const [currentPage, setcurrentPage] = useState<number>(1);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [currentDeck, setСurrentDeck] = useState<CurrentDeck>(
+    {} as CurrentDeck,
+  );
 
   const { data } = useGetDecksQuery({ name, currentPage });
   const [createDeck] = useCreateDeckMutation();
@@ -29,8 +38,18 @@ export const Decks = () => {
     createDeck({ name: "Temp test deck" });
   };
 
-  const onClickDeleteDeckButton = (id: string) => {
-    deleteDeck({ id });
+  const onClickDeleteDeckIcon = (id: string, name: string) => {
+    setСurrentDeck({ id, name });
+    setOpenModal(true);
+  };
+
+  const onClickDeleteDeckButton = () => {
+    deleteDeck({ id: currentDeck.id });
+    setOpenModal(false);
+  };
+
+  const onClickCloseButton = () => {
+    setOpenModal(false);
   };
 
   const onChangeSearchTextField = (e: ChangeEvent<HTMLInputElement>) => {
@@ -39,6 +58,30 @@ export const Decks = () => {
 
   return (
     <div className={s.pageDeck}>
+      <Modal
+        title={"Delete Deck"}
+        open={openModal}
+        onClose={onClickCloseButton}
+      >
+        <Typography className={s.textModal} variant="body2" as="span">
+          Do you really want to remove <b>Deck {currentDeck.name}?</b>
+          {"\n"}
+          All cards will be deleted.
+        </Typography>
+        <div className={s.blockButton}>
+          <Button variant="secondary" onClick={onClickCloseButton}>
+            <Typography variant="subtitle2" as="span">
+              Cancel
+            </Typography>
+          </Button>
+          <Button onClick={onClickDeleteDeckButton}>
+            <Typography variant="subtitle2" as="span">
+              Delete Pack
+            </Typography>
+          </Button>
+        </div>
+      </Modal>
+
       <TextField isSearch value={name} onChange={onChangeSearchTextField} />
       <Button className={s.button} onClick={onClickAddNewDeckButton}>
         <Typography variant="subtitle2" as="span">
@@ -66,7 +109,7 @@ export const Decks = () => {
               <TableCell>
                 <button
                   className={s.tempButton}
-                  onClick={() => onClickDeleteDeckButton(deck.id)}
+                  onClick={() => onClickDeleteDeckIcon(deck.id, deck.name)}
                 >
                   Delete
                 </button>
