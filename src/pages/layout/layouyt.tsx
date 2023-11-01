eb1cd4422b48144ac05f03e0b5c662a68f60656d
Алексей <73067946+Hacksways@users.noreply.github.com>
2023-11-01 11:22:49 +0000

    refactor: change align for forms

--- src/pages/layout/layouyt.tsx
+++ src/pages/layout/layouyt.tsx
@@ -1,18 +1,24 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
-import { Header } from 'components/ui/header'
-import { useLogoutMutation, useMeQuery } from 'services/auth/auth.service'
+import { Header } from "components/ui/header";
+import { useLogoutMutation, useMeQuery } from "services/auth/auth.service";
 
 export const Layout = () => {
-  const { data: user } = useMeQuery()
-  const [logout] = useLogoutMutation()
+  const { data: user } = useMeQuery();
+  const [logout] = useLogoutMutation();
 
   return (
     <>
-      <Header user={user} variant={'with avatar'} onSignOut={logout} />
-      <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Header user={user} variant={"with avatar"} onSignOut={logout} />
+      <main
+        // style={{
+        //   display: "flex",
+        //   justifyContent: "center",
+        //   alignItems: "center",
+        // }}
+      >
         <Outlet />
       </main>
     </>
-  )
-}
+  );
+};
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,27 +1,27 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
-import { SignIn } from 'components/auth'
-import { Typography } from 'components/ui/typography'
-import { useLoginMutation, useMeQuery } from 'services/auth/auth.service'
-import { LoginArgs } from 'services/auth/auth.types'
+import { SignIn } from "components/auth";
+import { Typography } from "components/ui/typography";
+import { useLoginMutation, useMeQuery } from "services/auth/auth.service";
+import { LoginArgs } from "services/auth/auth.types";
 
 export const Login = () => {
-  const { isLoading, isError } = useMeQuery()
-  const [login] = useLoginMutation()
-  const isAuth = !isError
+  const { isLoading, isError } = useMeQuery();
+  const [login] = useLoginMutation();
+  const isAuth = !isError;
   const loginHandler = (loginData: LoginArgs) => {
-    login(loginData)
-  }
+    login(loginData);
+  };
 
   if (isLoading) {
     return (
-      <Typography variant={'large'} as="h1">
+      <Typography variant={"large"} as="h1">
         Loading...
       </Typography>
-    )
+    );
   }
 
-  if (isAuth) return <Navigate to={'/'} replace={true} />
+  if (isAuth) return <Navigate to={"/"} replace={true} />;
 
-  return <SignIn onSubmit={loginHandler} />
-}
+  return <SignIn onSubmit={loginHandler} />;
+};
